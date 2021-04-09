a8062dd8ee430c167ca0078710c7dc1a86c7c6c4
tangbin <tonsonbin@gmail.com>
2021-04-09 18:49:48 +0800

    测试

--- src/main/java/com/tb/app/model/weixin/utils/Test.java
+++ src/main/java/com/tb/app/model/weixin/utils/Test.java
@@ -6,9 +6,9 @@ public class Test {
 	
 	public static void main(String[] args) {
 
-		WXDao wxDao = new WXDao().initSapp("wx1b08a5b0b90c4365", "edd5ce61f229bf13e4e4464a49c98468");
-		System.out.println(wxDao.wxspLogin("GLT9Y3Xq9I0BZ09jdgoXsGzA1QMPCSPlA97VX6/93V52jbSwy66tPUnZfGhGjjToNfoCHVZuWfEX8TPrByRi+kusHCG6YmOggaweSF2vh/M6uQNW/0DbnDjaq8Y2JTH9hKGjKC1hkLKywJUbKcnqscEDEZ3IRtuu/IEiBBqTAYlDLw6ES6eR3D3RDKvQdD4BC0Iz47MuzJk5ySwzkHciSO6przwvgtygnF4OxzRHcsMp/vXPH6trKehsfe+iA4t80ifwLwjlrOSXepuqDHTe65fNYv6TKIkjUpQtlJb4J/+gb9upwHRtbY13Pmk/1RLzbQtVokp5NVGAqhrNOtW431n4XhwE29u8iObyUoSdUDFTghgdupyKLCHXHQ7W5WJkBz0X7G4OaHqZ5LjviIYPzd3mq09VFtKko3Uh8oeVcpgarxYUdy6JSDXnsfPGqVQwk5qbqLyBMebAUTTXh5Oujg==",
-				"rSwq3w2t+j2y1rq9LA6uNg==", "071T2vFa1jBCPA0cRVFa1NyMCM2T2vF3"));
+		WXDao wxDao = new WXDao().initSapp("wx1b08a5b0b90c4365", "xxxx");
+		System.out.println(wxDao.wxspLogin("Xk3eSuZThNqJUYfPKkk42YC8kGWGmQYLnc+gXnW85Ei7aNWKMDLASzolj5PCPWpvNui85IzZsPBD258Q4ofr/v0ipQiS+LYqdgaKW1Kdf3C4OTC2nuDF+Bjf6V/xUnfGzC0LUTArRxzCzIZrP/NcXH7aEt2YAH+GB4A81/QWASkQZ7Bsr9adaxeKqjLHHVf130KTSAkQVottHAaz1VjNPXqsa9tXAEOqqLreiYd+1EID1wZYFSG2breDq+u4P35tAc8lByLDsQfrfvQXVE+TxhyV+YBU7DuS/m8i7N//Lq55glbH0XxIq9ZuY4jLd8CozNetjAwdHJSigM7vp+pZsxbo0UeVzbSeCdUHuZtg5zchwMA0vyot4xbHSgGm59+/Xrn7LqeqLohn3H7RBobDK1UPf2y5Yx48d4Sfg/EKk4qh7wlBRGhlLUgk8TZvUbgcNkpPDBXlmhp16swXazvoD==",
+				"VXlWV22RFmaymbI9YCRXzg==", "0313QK00070tvL1HUM0001eP3r23QK08"));
 		
 	}
 }
